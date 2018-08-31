99528d60718f178ff47d418a1eb82789a1d47a37
Klaus Klein <klaus.klein@cwi.com.br>
2018-08-31 17:00:13 -0300

    Base project implemented

---
+++ domain/src/main/java/com/github/klauswk/domain/Person.java
@@ -0,0 +1,20 @@
+package com.github.klauswk.domain;
+
+public class Person {
+	
+	private String name;
+	private Integer age;
+	
+	public String getName() {
+		return name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
